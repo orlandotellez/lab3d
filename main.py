7c4ed00a5a66560928a45b29a49dfbdc78ff2bf0
orlandotellez <espadachinkinno@gmail.com>
2026-05-10 12:19:02 -0600

    feat: cambiar color de fondo

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from src.room.floor import create_floor
 from src.room.walls import create_walls
 from src.objects.computer import create_lab_layout
 from src.lighting import create_lighting
+from src.ui.compass import create_compass
 from entities.player import create_player
 
 # =========================
@@ -21,17 +22,17 @@ window.title = 'Laboratorio 3D'
 window.borderless = False
 window.exit_button.visible = False
 window.fps_counter.enabled = True
-
+window.color = color.hex('#CBCBCB')
 
 # =========================
 # ESCENA
 # =========================
-
 create_floor()
 create_walls()
 create_lab_layout()
 create_lighting()
 create_player()
+create_compass()
 
 
 # =========================
