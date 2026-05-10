ab4d53e508d421da3cdb3531d62cd600dbce4081
orlandotellez <espadachinkinno@gmail.com>
2026-05-10 11:56:06 -0600

    feat: agregar pizarra

--- src/room/walls.py
+++ src/room/walls.py
@@ -7,6 +7,7 @@ from src.constants import AZUL_PARED, AZUL_VENTANA, COLOR_CORTINA, COLOR_TV
 COLOR_MARCO = color.black
 COLOR_PARED = color.hex('#5b667d')
 COLOR_CORITNAS = color.hex('#7f716e')
+COLOR_PIZARRA = color.hex('#b8b6b7')
 
 def create_window(z_center, wall_x, sign=1):
     face_x = wall_x - sign * 0.5
@@ -52,6 +53,17 @@ def create_tv(x_center, wall_z, sign=1):
 def create_front_tv():
     create_tv(-5, 10, sign=1)   # ← llama a create_tv, centrada en x=0
 
+def create_blackboard(x_center, wall_z, sign=1):
+    face_z = wall_z - sign * 0.5
+    z      = face_z - sign * 0.03
+    Entity(model='cube', 
+           scale=(7, 3.8, 0.05),
+           position=(x_center, 3.5, z), color=COLOR_PIZARRA)
+
+def create_front_blackboard():
+    create_blackboard(3, 11.8, sign=1)   # ← llama a create_tv, centrada en x=0
+
+
 def create_walls():
     Entity(model='cube', scale=(18,6,1), position=(0,3,12),
            color=AZUL_PARED, collider='box')
@@ -60,6 +72,7 @@ def create_walls():
     create_side_door(7.8, sign=-1)
     create_front_back_curtains()
     create_front_tv()
+    create_front_blackboard()
     Entity(model='cube', scale=(1,6,24), position=(-9,3,0),
            color=AZUL_PARED, collider='box')
     create_side_window_row(-9, sign=-1)
